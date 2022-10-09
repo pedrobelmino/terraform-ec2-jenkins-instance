a2e555e6ae55dfbe71873179f8495cc9735e817d
Pedro Belmino Chaves Filho <cwi.pchaves@bv.com.br>
2022-10-09 20:08:15 -0300

    ajustes no readme

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ LAB para criação de container ec2 utilizando terraform
 **Criação de Service account**
 - Crie uma service account em https://us-east-1.console.aws.amazon.com/iamv2/home
 
-**Criação do par de chaves para acesso SSH**
+**Criação do par de chaves para acesso SSH, de preferência com o nome 'terraform'**
 - https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#KeyPairs:
+- Baixar arquivo pem 'terraform.pem' e deixar um nível acima da pasta do projeto
 
 **Planejamento**
 ```bash
@@ -25,6 +26,19 @@ terraform apply
 ```bash
 terraform destroy
 ```
+
+**Acesso ao container via ssh**
+```bash
+ssh -i "../terraform.pem" [IP]
+```
+
+**Conectado na instância EC2, Recuperar chave para acesso**
+```bash
+sudo docker logs jenkins  
+```
+
+**Acesso e configuração do jenkins**
+- Acessar http://[IP]:80
 
 **Faça bom proveito!**
 
--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "techtalks_terraform_ec2_1" {
+resource "aws_instance" "cwitalks_terraform_ec2_1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.medium"
   key_name = "terraform" # Insira o nome da chave criada antes.
-  subnet_id = "${aws_subnet.techtalks-subnet-public-1.id}"
+  subnet_id = "${aws_subnet.cwitalks-subnet-public-1.id}"
   vpc_security_group_ids = [aws_security_group.permitir_ssh_http.id]
   associate_public_ip_address = true
 
@@ -28,7 +28,7 @@ resource "aws_instance" "techtalks_terraform_ec2_1" {
       "sudo apt-get install docker.io -y",
       "sudo service docker start",
       "sudo chmod 666 /var/run/docker.sock",
-      "docker run -d -v /home/ubuntu/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 80:8080 -p 5000:5000 pedrobelmino/jenkins-with-docker:latest"
+      "docker run -d -v /home/ubuntu/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 80:8080 -p 5000:5000 pedrobelmino/jenkins-with-docker:latest --name jenkins"
     ]
   }
   connection {
@@ -41,7 +41,7 @@ resource "aws_instance" "techtalks_terraform_ec2_1" {
 
 
   tags = {
-    Name = "techtalksterraform"
+    Name = "cwitalksterraform"
  # Insira o nome da instância de sua preferência.
   }
 }
