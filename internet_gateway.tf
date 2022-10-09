801162becebeebb2be11bb52fd4f68e9786096d7
Pedro Belmino Chaves Filho <cwi.pchaves@bv.com.br>
2022-10-09 20:35:17 -0300

    ajustando recursos terraform

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -1,3 +1,4 @@
 resource "aws_internet_gateway" "techtalks-igw" {
+
     vpc_id = "${aws_vpc.techtalks-vpc.id}"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,7 @@
-resource "aws_vpc" "cwitalks-vpc" {
+resource "aws_vpc" "techtalks-vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_support = "true" 
     enable_dns_hostnames = "true" 
-    enable_classiclink = "false"
     instance_tenancy = "default"    
     # virtual private cloud
 }
